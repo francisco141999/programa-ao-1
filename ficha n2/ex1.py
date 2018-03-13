c9e1c6efdf541eee1986d5263fea79d3a67a24cb
francisco141999 <franciscoazevedo16@gmail.com>
2018-03-13 16:10:18 +0000

    fim do exercicio 1

--- ficha n2/ex1.py
+++ ficha n2/ex1.py
@@ -1,2 +1,10 @@
-numero1 = input("Ler um numero: ")
-numero2 = input("Ler um numero: ")
+numero1 = input("Introduza um numero: ")
+numero2 = input("Introduza um numero: ")
+  
+if numero1 == numero2:
+    print(numero1)
+elif numero1 > numero2:
+    print(numero1)
+else:
+    print(numero2)    
+    
