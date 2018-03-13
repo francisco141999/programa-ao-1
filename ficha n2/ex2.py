ba8b35992fec415ab991fe620dd268670bdac3c7
francisco141999 <franciscoazevedo16@gmail.com>
2018-03-13 16:17:13 +0000

    inicio do exercico 2

---
+++ ficha n2/ex2.py
@@ -0,0 +1,18 @@
+numero1 = input("Introduza um numero: ")
+numero2 = input("Introduza um numero: ")
+"""maior
+"""  
+if numero1 == numero2:
+    print(numero1)
+elif numero1 > numero2:
+    print(numero1)
+else:
+    print(numero2) 
+"""menor
+"""  
+if numero1 == numero2:
+    print(numero1)
+elif numero1 < numero2:
+    print(numero1)
+else:
+    print(numero2)     
