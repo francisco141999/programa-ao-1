a624878e226506412a82e78f673cfe6fbe3b5d3b
francisco141999 <franciscoazevedo16@gmail.com>
2018-03-20 15:52:38 +0000

    falta o exercicio 1

--- fichan2entraga/ex5.py
+++ fichan2entraga/ex5.py
@@ -1,15 +1,19 @@
-saldo = input ("O seu saldo da sua conta bancaria: ")
-debitar = input ("O saldo a debitar: ")
+saldo = float(input ("O seu saldo da sua conta bancaria: "))
+print("Operaçao que quer realizar: ")
+print ('1-deposito')
+print ('2-debito')
+operacao = input("Introduzaa operaçao que quer realizar: ")
 
-
-
-if saldo > debitar:
-    print("o debito foi realizado com sucesso.")
-elif saldo < debitar:
-    print("Operaçao impossivel por saldo insuficiente.")
-
-dinheiro = saldo - debitar
-print("Ainda tem {}".format(dinheiro))
-
+if operacao == '1' :
+    deposito = float(input ("O Deposito na sua conta: "))
+    valor = saldo + deposito
+    print('Ficou com um saldo de: ', valor)
+elif operacao == '2' : 
+    debito = float(input ("O dinheiro a debitar: "))
+    valor = saldo - debito
+    if valor < 0 :
+        print('Operaçao impossivel por saldo insuficiente')
+    else :
+        print('Ficou com um saldo de: ', valor)
 
 
