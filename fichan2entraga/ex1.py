25c115e00fa460315c8181e2427b53a78570ce06
francisco141999 <franciscoazevedo16@gmail.com>
2018-04-01 12:50:34 +0100

    conclusao da ficha n2 para entrega

--- fichan2entraga/ex1.py
+++ fichan2entraga/ex1.py
@@ -1,17 +1,26 @@
+pnota1= .25
+pnota2= .35
+pnota3= .40
 
+#Nota1
+nota1 = float(input("Introduza a primeira nota: "))
+while nota1 < 0.0 or nota1 > 20.0:
+    nota1 = float(input("Introduza a primeira nota: "))
 
+#Nota2
+nota2 = float(input("Introduza a segunda nota: "))
+while nota2 < 0.0 or nota2 > 20.0:
+    nota2 = float(input("Introduza a segunda nota: "))
 
-if (nota1 < 0 and nota1 > 20) or (nota2 < 0 and nota2 > 20) or (nota3 < 0 and nota3 > 20); 
-    nota1 = float(input("Introduza o valor da primeira nota (0-20): "))
-    nota2 = float(input("Introduza o valor da segunda nota (0-20): "))
-    nota3 = float(input("Introduza o valor da terceira nota (0-20): "))
-else:
-    media = float((nota1 * 25 + nota2 * 35 + nota3 * 40) / 100)
+#Nota3
+nota3 = float(input("Introduza a terceira nota: "))
+while nota3 < 0.0 or nota3 > 20.0:
+    nota3 = float(input("Introduza a terceira nota: "))
 
-if media >= 9,5:
-    print("aprovado")
-else:
-    print("reprovado")
-        
-   
+#Média
+media = int((nota1 * pnota1 + nota2 * pnota2 + nota3 * pnota3))
 
+if media >= 9.5 :
+    print("Aprovado com {} (valor arredondado)".format(media))
+else :
+    print("Reprovado com {} (valor arredondado)".format(media))
