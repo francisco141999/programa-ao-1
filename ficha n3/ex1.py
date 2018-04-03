e5fa1d3e88ee3fcfb6d3e3fb5d02d39775aa4115
francisco141999 <franciscoazevedo16@gmail.com>
2018-04-03 15:13:48 +0100

    conclusao da ficha n3 ex1

--- ficha n3/ex1.py
+++ ficha n3/ex1.py
@@ -1,1 +1,5 @@
-numero=int(input())
+numero=int(input("Introduza o numero de asteriscos: "))
+
+print("Os 5 números anteriores a {} são: ".format(numero))
+for i in range(numero-1, numero-6, -1):
+    print(i, end=" ")
