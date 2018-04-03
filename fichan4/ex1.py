cad8e86abf701cac05370d406e68433a6ea19eeb
francisco141999 <franciscoazevedo16@gmail.com>
2018-04-03 15:48:21 +0100

    meio da ficha n4 ex2

---
+++ fichan4/ex1.py
@@ -0,0 +1,9 @@
+"""
+exercicio 1
+"""
+
+def print_line(valor):
+    print("*" * valor)
+
+numero=int(input("Introduza um numero: "))
+print(numero)
---
+++ fichan4/ex2.py
@@ -0,0 +1,34 @@
+def soma(numero1,numero2):
+    return numero1 + numero2
+
+def subtraçao(numero1,numero2):
+    return numero1 - numero2
+
+def divisao(numero1,numero2):
+    return numero1 / numero2
+
+def multiplicacao(numero1,numero2):
+    return numero1 * numero2  
+
+
+numero1 = int(input("Introduza o primeiro numero: "))
+
+numero2 = int(input("Introduza o segundo numero: "))
+
+operaçao=input("Introduza a operacao (+),(-),(*) e (/): ")
+
+if operaçao == "+"
+    soma(numero1,numero2)
+    print("{} = {} + {}".format(numero1,numero2,soma)
+
+elif  operaçao == "-"
+    subtraçao(numero1,numero2)
+    print("{} = {} - {}".format(numero1,numero2,subtraçao)  
+
+elif  operaçao == "/"
+    divisao(numero1,numero2)
+    print("{} = {} - {}".format(numero1,numero2,divisao)    
+
+elif  operaçao == "*"
+    multiplicacao(numero1,numero2)
+    print("{} = {} - {}".format(numero1,numero2,multiplicacao)               
